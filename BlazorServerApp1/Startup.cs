85619cb1ed390785b4101c6c11787e0c4983c0f3
proneko <producerneko@hotmail.com>
2023-04-26 18:57:04 +0900

    0426

---
+++ BlazorServerApp1/Startup.cs
@@ -0,0 +1,85 @@
+﻿using BlazorServerApp1.Data;
+using BlazorServerApp1.Hubs;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+//namespace BlazorServerApp1
+//{
+//    public class Startup
+//    {
+//        public Startup(IConfiguration configuration)
+//        {
+//            Configuration = configuration;
+//        }
+
+//        public IConfiguration Configuration { get; }
+
+//        // This method gets called by the runtime. Use this method to add services to the container.
+//        public void ConfigureServices(IServiceCollection services)
+//        {
+//            services.AddRazorPages();
+//            services.AddServerSideBlazor();
+//            services.AddSingleton<WeatherForecastService>();
+//        }
+
+//        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+//        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+//        {
+//            if (env.IsDevelopment())
+//            {
+//                app.UseDeveloperExceptionPage();
+//            }
+//            else
+//            {
+//                app.UseExceptionHandler("/Error");
+//                app.UseHsts();
+//            }
+
+//            app.UseHttpsRedirection();
+//            app.UseStaticFiles();
+
+//            app.UseRouting();
+
+//            app.UseEndpoints(endpoints =>
+//            {
+//                endpoints.MapBlazorHub();
+//                endpoints.MapFallbackToPage("/_Host");
+//            });
+//        }
+//    }
+//}
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace BlazorServerApp1
+//BlazorSignalRApp
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHub<ChatHub>("/chatHub");
+                endpoints.MapFallbackToFile("index.html");
+            });
+        }
+    }
+}
+
---
+++ BlazorServerApp1/chatHub.cs
@@ -0,0 +1,21 @@
+﻿//using Microsoft.AspNetCore.SignalR;
+
+//namespace BlazorServerApp1
+//{
+//    public class ChatHub
+//    {
+//    }
+//}
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+//namespace BlazorSignalRApp.Hubs
+namespace BlazorServerApp1.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
